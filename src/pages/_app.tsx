6e2c8b45f9ca9fefc1f98ef068b119bdf8711f5e
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-10-05 00:17:40 +0530

    feat: Cleaning

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,7 @@
 /** @format */
 
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-// pages/_app.tsx
-
-// pages/_app.tsx
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
